2e68b99240e576e05d320e6dbe0363fd6ddd50b9
blueyi <blueyi>
2016-12-02 22:25:46 +0800

    rebuild all function and add create original vm function

--- README.md
+++ README.md
@@ -5,7 +5,6 @@ export PATH=$PATH:~/kvmManage
 add to your .bashrc or .bash_profile or /etc/profile
 
 
-
 虚拟机相关的管理命令如下，为了方便调用，所以都以dut开头，可以通过输出dut之后双击tab来查看你需要的相关命令，如果有其他功能需求，可以向我提：
 
 dut-createvm <your-host-name> 创建名为<your-host-name>的虚拟机，统一规定自己创建的虚拟机以自己的姓名简拼开头，只能是英文，数字，短划线的组合，如wyl-master，暂时默认只创建ubuntu server 16.04版本的虚拟机1G内存，1核CPU。虚拟机创建完之后会给你一个VNC端口号，可以通过VNC访问你的虚拟机。同时会给出IP和SSH端口号，通过ssh访问，SSH端口是22，默认禁用root登录。虚拟机的默认用户名和密码都是dutoe，自行修改。默认关闭防火墙。该命令需要sudo权限验证。
@@ -25,18 +24,15 @@ dut-shutdownvm  <your-host-name>  强制关闭虚拟机
 
 dut-startvm  <your-host-name> 手动启动虚拟机
 
-dut-vmVNCPort  <your-host-name>  查看虚拟机的VNC端口号，只有虚拟机运行期间才会有VNC端口号
+dut-getvminfo  <your-host-name>  查看虚拟机的基本信息，包括IP和VNC端口号，只有虚拟机运行期间才会有IP和VNC端口号
 
-dut-getvmip  <your-host-name> 查看虚拟机IP
+dut-clonevmfrom  <your-clone-from-host-name> <new-host-name> 克隆虚拟机，第一个参数是被克隆的虚拟机名字，第二个参数是新克隆出来的虚拟机名字
 
-通常情况下IP不会变化，为了防止重启后IP变化，记得通过dut-getvmip查看你的虚拟机IP，可以联系我在路由器中绑定你的虚拟机MAC跟IP。
+dut-originalvmcreate  创建你指定的内在大小、CPU个数、硬盘大小和系统光盘路径的虚拟机
+
+通常情况下IP不会变化，为了防止重启后IP变化，记得通过dut-getvminfo查看你的虚拟机IP，可以联系我在路由器中绑定你的虚拟机MAC跟IP。
 
 VNC连接方法：
-VNC需要通过SSH tunnel才能连接虚拟机，所以都需要建立SSH tunnel连接
-Windows系统：
-1.从REALVNC官方网站 https://www.realvnc.com/download/viewer/下载VNC viewer安装。
-2.打开putty，Host name中输入服务器IP（192.168.1.121），端口默认22。点开左侧的SSH选项，点击Tunnels，source port中输入你想使用的本地端口，Destination中输入你需要建立隧道的本地IP和远程虚拟机的VNC监听端口，点击Add后，Open即可，此时需要输入服务器用户和密码（dutoeserver）。这样就建立好了SSH隧道。如果有多台虚拟机，则通过add一次可以添加多个监听隧道。如我有虚拟机3台，分别端口为5901、5902、5903，则我可以Source port中填写5901，然后Destination中填写127.0.0.1:5901，点击Add，其他两个端口类似添加。最后点击Open即可
-3.打开VNC viewer，输入127.0.0.1:5901即可连接5901对应的虚拟机，可以同时连接多个虚拟机
-暂时没有找到好的方法在不进入虚拟机的情况下获取其IP
+1.从REALVNC官方网站 https://www.realvnc.com/download/viewer/下载VNC viewer安装，支持Windows和Linux
+3.打开VNC viewer，输入192.168.1.121:5901即可连接5901对应的虚拟机，可以同时连接多个虚拟机
 
-服务器用户名dutoeserver，密码dutoeserver，服务器IP为192.168.1.121。该账号仅用于虚拟机相关操作
--- common.py
+++ common.py
@@ -8,13 +8,16 @@
 
 import subprocess
 import sys
+import time
 
 """
 Some function and const value
 """
 
 image_path = '/home/blueyi/vhost/image/'
-iso_path = 'home/blueyi/vhost/iso/'
+iso_path = '/home/blueyi/vhost/iso/'
+template_kvm = 'ubuntu1604'
+sub_ip = '192.168.2.0'
 
 
 # print some important string
@@ -24,7 +27,7 @@ def welcome_print(msg):
     print('*' * 70)
 
 # run an shell command in subprocess
-def run_cmd_reout(tcall_cmd, goOnRun = False, isOutPut = True, jumpErr = False, isRetuenCode = False):
+def run_cmd_reout(tcall_cmd, goOnRun = False, isOutPut = True, jumpErr = False, isReturnCode = False):
     p = subprocess.Popen(tcall_cmd, shell=True, stdout=subprocess.PIPE, executable='/bin/bash')
     toutput = p.communicate()[0]
     if p.returncode != 0 and not jumpErr:
@@ -33,15 +36,22 @@ def run_cmd_reout(tcall_cmd, goOnRun = False, isOutPut = True, jumpErr = False, 
             sys.exit(1)
     if isOutPut :
         print(toutput)
-    if isRetuenCode :
+    if isReturnCode :
         return p.returncode
     else :
         return toutput
 
+
+# Is specified KVM exist
+def isVMExist(host_name) :
+    is_exist_cmd = 'virsh domstate ' + host_name
+    return (run_cmd_reout(is_exist_cmd, goOnRun=True, isOutPut=False, jumpErr=True, isReturnCode=True) == 0)
+
+
 # Is specified KVM running
 def isVMRunning(host_name) :
     is_run_cmd = 'virsh domstate ' + host_name
-    return (run_cmd_reout(is_run_cmd, goOnRun=True, isOutPut=False, jumpErr=True, isRetuenCode=True) == 0) and (run_cmd_reout(is_run_cmd, goOnRun=True, isOutPut=False, jumpErr=True).strip() == 'running')
+    return isVMExist(host_name) and (run_cmd_reout(is_run_cmd, goOnRun=True, isOutPut=False, jumpErr=True).strip() == 'running')
 
 # list all kvm exist
 def listAllKVM() :
@@ -50,8 +60,155 @@ def listAllKVM() :
 
 # Get VNC port
 def getVNCPort(host_name) :
+    if not isVMRunning(host_name) :
+        return host_name + ' Not Running!'
     vnc_display_cmd = 'virsh vncdisplay ' + host_name
-    vnc_reout = run_cmd_reout(vnc_display_cmd, isOutPut=False)
+    vnc_reout = run_cmd_reout(vnc_display_cmd, goOnRun=True, isOutPut=False, jumpErr=True)
+    vnc_port = 5900 + int(vnc_reout[(vnc_reout.find(':') + 1) : ])
+    return str(vnc_port)
+
+# Get MAC address
+def getMAC(host_name) :
+    if not isVMExist(host_name) :
+        return host_name + ' Not Exist!'
+    get_mac_cmd = "virsh dumpxml " + host_name + " | grep 'mac address' | cut -c 21-37"
+    return run_cmd_reout(get_mac_cmd, goOnRun=True, isOutPut=False).strip()
+
+# Get ip address
+def getIP(host_name) :
+    if not isVMRunning(host_name) :
+        return host_name + ' Not Running!'
+    host_mac = getMAC(host_name)
+
+    arp_ip = ''
+    start_time = int(time.time())
+    # wait for kvm start finish
+    while (arp_ip == '' and int(time.time()) - start_time < 20):
+        nmap_cmd = 'nmap -sP --host-timeout 15s ' + sub_ip + '/24'
+        run_cmd_reout(nmap_cmd, goOnRun=True, isOutPut=False)
+        arp_ip_cmd = "arp -an | grep '"  + host_mac + "'"
+        arp_ip = run_cmd_reout(arp_ip_cmd, goOnRun=True, isOutPut=False, jumpErr=True)
+    if len(arp_ip) == 0 :
+        return 'Get ' + host_name + ' ip address failed! MAC:' + host_mac
+    else :
+        return arp_ip[arp_ip.find('(') + 1 : arp_ip.find(')')]
+
+# get specified KVM info, mac, ip and vnc port
+def getVMInfo(host_name) : 
+    if not isVMExist(host_name) :
+        print('<<< ' + host_name + ' Not Exist! >>>')
+        return
+    print('------' + host_name + ' infomation------')
+    host_info_cmd = 'virsh dominfo ' + host_name
+    run_cmd_reout(host_info_cmd)
+    print('MAC address: ' + getMAC(host_name))
+    welcome_print(host_name + ' IP: ' + getIP(host_name) + ', VNC port: ' + getVNCPort(host_name) +  ' enjoy it!')
+
+# Start kvm
+def startVM(host_name) :
+    if not isVMExist(host_name) :
+        print('<<< ' + host_name + ' Not Exist! >>>')
+        listAllKVM()
+        return
+    if isVMRunning(host_name) :
+        print('<<< ' + host_name + ' has Running! >>>')
+        listAllKVM()
+        return
+    start_cmd = 'virsh start ' + host_name
+    run_cmd_reout(start_cmd)
+    getVMInfo(host_name)
+
+# Shutdown kvm
+def shutdownVM(host_name) :
+    if not isVMExist(host_name) :
+        print('<<< ' + host_name + ' Not Exist! >>>')
+        listAllKVM()
+        return
+    if not isVMRunning(host_name) :
+        print('<<< ' + host_name + ' Not Running! >>>')
+        listAllKVM()
+        return
+    shutdown_cmd = 'virsh shutdown ' + host_name
+    run_cmd_reout(shutdown_cmd)
+
+# Set specified KVM to autostart when boot
+def autostartVM(host_name) :
+    if not isVMExist(host_name) :
+        print('<<< ' + host_name + ' Not Exist! >>>')
+        listAllKVM()
+        return
+    autostart_cmd = 'virsh autostart ' + host_name
+    run_cmd_reout(autostart_cmd)
+    # list all kvm exist status
+    print('Autostart virtual machine list in the following')
+    run_cmd_reout('virsh list --autostart --all')
+
+# disable autostart kvm
+def disableAutostartVM(host_name) :
+    if not isVMExist(host_name) :
+        print('<<< ' + host_name + ' Not Exist! >>>')
+        listAllKVM()
+        return
+    autostart_cmd = 'virsh autostart --disable ' + host_name
+    run_cmd_reout(autostart_cmd)
+    # list all kvm exist status
+    print('Autostart virtual machine list in the following')
+    run_cmd_reout('virsh list --autostart --all')
+
+# Clone kvm from template
+def cloneVM(host_name, template) :
+    if isVMRunning(template) :
+        print(template + ' must to be shutdown to be clone!')
+        return
+    clone_cmd = 'virt-clone --force --original=' + template + ' --name=' + host_name + ' --file=' + image_path + host_name + '.qcow2'
+    run_cmd_reout(clone_cmd)
+
+# Sysprep kvm
+def sysprepVM(new_host_name, old_host_name, miniClear = False) :
+    if isVMRunning(old_host_name) :
+        print(old_host_name + ' must to be shutdown to be clone!')
+        return
+    if isVMExist(new_host_name) :
+        print(new_host_name + ' has exist!')
+        return
+    sysprep_cmd = ''
+    if miniClear :
+        sysprep_cmd = 'sudo virt-sysprep -d ' + old_host_name + ' --hostname ' + new_host_name + ' --operations customize,dhcp-client-state,dhcp-server-state,machine-id,net-hostname,net-hwaddr'
+    else :
+        sysprep_cmd = 'sudo virt-sysprep -d ' + old_host_name + ' --hostname ' + new_host_name + ' --operations defaults,-ssh-hostkeys'
+    run_cmd_reout(sysprep_cmd)
 
 
+# Remove specified vm define
+def undefineVM(host_name) :
+    if isVMRunning(host_name) :
+        shutdownVM(host_name)
+    undefine_cmd = 'virsh undefine ' + host_name
+    run_cmd_reout(undefine_cmd)
+
+
+# Delete specified vm from disk
+def deleteVM(host_name) :
+    undefineVM(host_name)
+    delete_cmd = 'sudo rm -rf ' + image_path + host_name + '.qcow2'
+    run_cmd_reout(delete_cmd)
+
+
+# Originally create kvm from iso image
+def createVM(host_name, ram = 1024, vcpu = 1, disk = 10, os_type = 'ubuntu16.04', iso = 'UbuntuServer16.04.iso') :
+    if isVMExist(host_name) :
+        print(host_name + ' has exist!')
+        return
+    create_cmd = 'virt-install \
+            --virt-type=kvm \
+            --name=' + host_name + ' \
+            --ram=' + str(ram) + ' \
+            --vcpus=' + str(vcpu) + ' \
+            --os-variant=' + os_type + ' \
+            --hvm \
+            --cdrom=' + iso_path + iso + ' \
+            --network=bridge=br0,model=virtio \
+            --graphics vnc,listen=0.0.0.0 \
+            --disk path=' + image_path + host_name + '.qcow2,size=' + str(disk) + ',bus=virtio,format=raw'
+    run_cmd_reout(create_cmd)
 
